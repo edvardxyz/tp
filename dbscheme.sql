b8cb5b1b490e5cfc34a1987a548803d7665a8c20
edvard <edvard@space-inventor.com>
2025-03-07 15:15:35 +0100

    added login to dbscheme

--- dbscheme.sql
+++ dbscheme.sql
@@ -1,7 +1,6 @@
 CREATE TABLE node (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
 	node INTEGER NOT NULL,
+	name TEXT NOT NULL,
 	created INTEGER NOT NULL DEFAULT (strftime('%s','now'))
 );
 
@@ -31,7 +30,8 @@ CREATE TABLE param_node (
 	node_id INTEGER NOT NULL,
 	created INTEGER NOT NULL DEFAULT (strftime('%s','now')),
 	FOREIGN KEY(param_id) REFERENCES param(id),
-	FOREIGN KEY(node_id) REFERENCES node(id)
+	FOREIGN KEY(node_id) REFERENCES node(node),
+	UNIQUE (param_id, node_id)
 );
 
 CREATE TABLE param_int (
@@ -59,19 +59,32 @@ CREATE TABLE pushed (
 	node_id INTEGER NOT NULL,
 	val_id INTEGER NOT NULL,
 	created INTEGER NOT NULL DEFAULT (strftime('%s','now')),
-	FOREIGN KEY(node_id) REFERENCES node(id)
+	FOREIGN KEY(node_id) REFERENCES node(node)
 );
 
 CREATE TABLE contact (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	node_to_id INTEGER NOT NULL,
 	node_from_id INTEGER NOT NULL,
-	created INTEGER NOT NULL DEFAULT (strftime('%s','now')),
-	FOREIGN KEY(node_to_id) REFERENCES node(id),
-	FOREIGN KEY(node_from_id) REFERENCES node(id)
+	time_sec INTEGER NOT NULL,
+	FOREIGN KEY(node_to_id) REFERENCES node(node),
+	FOREIGN KEY(node_from_id) REFERENCES node(node)
 );
 
-CREATE INDEX idx_param_node_param ON param_node(param_id);
-CREATE INDEX idx_param_node_node ON param_node(node_id);
+CREATE TABLE login (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user TEXT NOT NULL,
+	hash TEXT NOT NULL,
+	created INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+	UNIQUE (user)
+);
+
+
+
+CREATE INDEX idx_param_node_param ON param_node(id);
 CREATE INDEX idx_param_int_param_node ON param_int(param_node_id);
 CREATE INDEX idx_param_real_param_node ON param_real(param_node_id);
+
+INSERT INTO subnet (name, node, mask) VALUES ('sub1', 0, 8);
+INSERT INTO subnet (name, node, mask) VALUES ('sub2', 64, 8);
+INSERT INTO subnet (name, node, mask) VALUES ('sub3', 128, 8);
