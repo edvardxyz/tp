eb08d301e7bf6891b137551f16bce0b8d8ae5047
edvard <edvard@space-inventor.com>
2025-03-07 16:02:45 +0100

    node api filter added

--- c-web-api/src/node.c
+++ c-web-api/src/node.c
@@ -8,9 +8,23 @@
 #include "db.h"
 
 int callback_get_nodes(const struct _u_request * request, struct _u_response * response, void * user_data) {
-	const char * sql = "SELECT id, name, node, FROM node;";
 
-	json_t * json_array = fetch_query_results(sql, "");
+	const char * sql = NULL;
+	const char * node = u_map_get(request->map_url, "node");
+	json_t * json_array = NULL;
+
+	if (node) {
+		sql = "SELECT name, node FROM node WHERE node = ?;";
+		json_array = fetch_query_results(sql, "%s", node);
+	} else {
+		sql = "SELECT name, node FROM node;";
+		json_array = fetch_query_results(sql, "");
+	}
+
+	if (!json_array) {
+		ulfius_set_json_body_response(response, 500, json_pack("{s:s}", "error", "Failed to fetch nodes"));
+		return U_CALLBACK_CONTINUE;
+	}
 
 	// Convert the JSON array to a string for the response body
 	char * json_str = json_dumps(json_array, JSON_INDENT(2));
