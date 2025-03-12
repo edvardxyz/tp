885508c999304c4f4174952bea25c66b9e4c7a80
edvard <edvard@space-inventor.com>
2025-03-12 13:58:24 +0100

    auth fixing

--- c-web-api/src/auth.c
+++ c-web-api/src/auth.c
@@ -9,6 +9,36 @@ static const unsigned char JWT_SECRET_KEY[] = "secret32329";
 #include "db.h"
 #include <bcrypt.h>
 
+char * print_map(const struct _u_map * map) {
+	char *line, *to_return = NULL;
+	const char **keys, *value;
+	int len, i;
+	if (map != NULL) {
+		keys = u_map_enum_keys(map);
+		for (i = 0; keys[i] != NULL; i++) {
+			value = u_map_get(map, keys[i]);
+			len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
+			line = o_malloc((size_t)(len + 1));
+			snprintf(line, (size_t)(len + 1), "key is %s, value is %s", keys[i], value);
+			if (to_return != NULL) {
+				len = (int)(o_strlen(to_return) + o_strlen(line) + 1);
+				to_return = o_realloc(to_return, (size_t)(len + 1));
+				if (o_strlen(to_return) > 0) {
+					strcat(to_return, "\n");
+				}
+			} else {
+				to_return = o_malloc((o_strlen(line) + 1));
+				to_return[0] = 0;
+			}
+			strcat(to_return, line);
+			o_free(line);
+		}
+		return to_return;
+	} else {
+		return NULL;
+	}
+}
+
 char * create_jwt_hs256(const char * username);
 
 char * create_jwt_hs256_refresh(const char * username) {
@@ -341,6 +371,8 @@ int callback_auth(const struct _u_request * request, struct _u_response * respon
 
 	const char * auth = u_map_get(request->map_header, "Authorization");
 	const char * token = NULL;
+
+	print_map(request->map_header);
 
 	if (strcmp(request->url_path, "/wsprint") == 0) {
 		printf("Authentication for websocket endpoint.\n");
